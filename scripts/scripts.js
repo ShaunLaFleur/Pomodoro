7bb6dc7f2c57df04ca0a2698cb10a47650e945fb
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2017-02-04 00:01:07 -0600

    Initial Commit

---
+++ scripts/scripts.js
@@ -0,0 +1,89 @@
+var sessionTime = 60,
+	breakTime = 60,
+	sessionOn = true,
+	clockStart = false,
+	startTimer,
+	seconds = 60;
+
+$("#start").click(function(){
+	if(!clockStart) {
+		startTimer = setInterval(timeOn, 1000);
+		clockStart = true;
+		$("#start").html("<h3>Pause</h3>");
+		$("input").prop("disabled", true);
+	} else {
+		clearInterval(startTimer);
+		clockStart = false;
+		$("#start").html("<h3>Start</h3>");
+	}
+});
+
+$("#reset").click(function(){
+	$("#work-hours, #rest-hours, #work-sec, #rest-sec").val(0);
+	$("#work-min, #rest-min").val(1);
+	sessionTime = 60;
+	breakTime = 60;
+	seconds = 60;
+	clockStart = false;
+	clearInterval(startTimer);
+	$("#phase").html("&nbsp;");
+	$("#start").html("<h3>Start</h3>");
+	$("#hours, #minutes, #seconds").html("00");
+	$("input").prop("disabled", false);
+});
+
+
+$("input").change(function(){
+	if($(this).val() < 0) {
+		$(this).val(0);
+	} else if($(this).val() >= 60 && $(this).data("hours") !== "yes") {
+		$(this).val(59);
+	}
+
+	if($(this).attr("class") === "session") {
+		var hours = $("#work-hours").val()*60*60;
+		var minutes = $("#work-min").val()*60;
+		var sec = parseInt($("#work-sec").val());
+		sessionTime = hours+minutes+sec;
+		seconds = sessionTime;
+	} else {
+		var hours = $("#rest-hours").val()*60*60;
+		var minutes = $("#rest-min").val()*60;
+		var sec = parseInt($("#rest-sec").val());
+		breakTime = hours+minutes+sec;
+	}
+});
+
+function timeOn() {
+	if($("#phase").html() === "&nbsp;") {
+			$("#phase").html("Session");
+		}
+	if(seconds === -1) {
+		if(sessionOn) {
+			sessionOn = false;
+			seconds = breakTime;
+			console.log("Break time");
+			$("#phase").html("Break");
+		} else {
+			sessionOn = true;
+			seconds = sessionTime;
+			console.log("Session time");
+			$("#phase").html("Session");
+		}
+	}
+	var hour = Math.floor(seconds/3600);
+	var min = Math.floor(seconds/60%60);
+	var sec = seconds%60;
+	$("#hours").html(zero(hour)+hour);
+	$("#minutes").html(zero(min)+min);
+	$("#seconds").html(zero(sec)+sec);
+	seconds--;
+}
+
+function zero(n) {
+	if(n < 10) {
+		return "0";
+	} else {
+		return "";
+	}
+}
